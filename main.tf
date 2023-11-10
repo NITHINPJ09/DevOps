063bda5aa8410d77fee86d7d42e8a6a55d9b3115
Nithin Prince John <nithinpj09@gmail.com>
2023-11-10 12:23:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,8 +42,8 @@ resource "aws_instance" "demo" {
 
 resource "aws_security_group" "instance_sg" {
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
